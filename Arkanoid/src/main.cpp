3ca341ab6ff1759fff37a16d09a8c85e2de5dc5d
Matías Houston <matias.houston.r@gmail.com>
2025-02-26 20:57:19 -0300

    Funciona?

--- Arkanoid/src/main.cpp
+++ Arkanoid/src/main.cpp
@@ -3,27 +3,4 @@
 #include "Adafruit_GFX.h"
 #include <Adafruit_SSD1306.h>
 
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-  display.display();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-  display.display();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}   efee      
-egewrg
+"se carga o no?"
